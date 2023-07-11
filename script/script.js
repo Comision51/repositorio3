cbd8e1ad131a31d076e469d93dd79c5dba38d174
Comision51 <119951123+Comision51@users.noreply.github.com>
2023-07-11 16:34:48 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+var opcion=Number(prompt("ingrese que desea observar 1-series 2-peliculas 3-videos"));
+
+switch(opcion){
+    case 1:
+          alert("usted selecciono la opcion series...iniciando")
+          break;
+    case 2:
+          alert("usted selecciono la opcion... peliculas")
+          break;
+    case 3:
+          alert("usted selecciono la opcion...videos")
+          break;
+    default:
+        alert("no selecciono la opcion correcta")
+}
